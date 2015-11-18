b7273cbbfa8e8525b413783d2e00fd19de0fb09c
Sean Esteva <seangeleno@gmail.com>
2015-11-18 14:40:39 -0800

    init commit

---
+++ app.js
@@ -0,0 +1,40 @@
+var
+	express = require('express'),
+	app = express(),
+	ejs = require('ejs'),
+	ejsLayouts = require('express-ejs-layouts'),
+	mongoose = require('mongoose'),
+	flash = require('connect-flash'),
+	logger = require('morgan'),
+	cookieParser = require('cookie-parser'),
+	bodyParser = require('body-parser'),
+	session = require('express-session'),
+	passport = require('passport')
+
+// environment port
+var port = process.env.PORT || 3000
+
+// mongoose connection
+mongoose.connect('mongodb://localhost/passport-authentication', function(err){
+	if(err) return console.log('Cannot connect :(')
+	console.log('Connected to MongoDB. Sweet!')
+})
+
+// middleware
+app.use(logger('dev'))
+app.use(cookieParser())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+// ejs configuration
+app.set('view engine', 'ejs')
+app.use(ejsLayouts)
+
+//root route
+app.get('/', function(req,res){
+	res.render('index')
+})
+
+app.listen(port, function(){
+	console.log("Server running on port", port)
+})
