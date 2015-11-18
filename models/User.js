d2acbebe2172d126e6140b2e0322e2e1ab63c5e0
Sean Esteva <seangeleno@gmail.com>
2015-11-18 15:03:25 -0800

    added schema

--- models/User.js
+++ models/User.js
@@ -1,1 +1,19 @@
 //user model
+var mongoose       = require('mongoose')
+,   bcrypt         = require('bcrypt-nodejs')
+,   Schema         = mongoose.Schema
+
+var userSchema = new Schema({
+  local: {
+    name:     String
+,   email:    String
+,   password: String
+  }
+,
+  facebook: {
+    id:    String
+,   name:  String
+,   token: String
+,   email: String
+  }
+})
