145c49eba0c9ee3b4c8b03ff9e432886b6222f2e
Sean Esteva <seangeleno@gmail.com>
2015-11-18 16:11:18 -0800

    finished passport

--- config/passport.js
+++ config/passport.js
@@ -11,3 +11,39 @@ passport.deserializeUser(function(id, done){
     done(err, user)
   })
 })
+//////////////SIGNUP-SIGNUP-SIGNUP////////////////////////////
+passport.use('local-signup', new LocalStrategy({
+//we can overwrite what passport usually wants, username, and add email as an alternative
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: true,
+  function(req, email, password, done){
+    User.findOne({'local.email': email},function(err, user){
+      if(err)return done(err)
+      if(user)return done(nll, false, req.flash('signupMessage','That email is already taken'))
+      var newUser = new User()
+      newUser.local.name = req.body.name
+      newUser.local.email = email
+      newUser.local.password = newUser.generateHash(password)
+
+      newUser.save(function(err){
+        if(err)throw console.error(return done(null, newUser));
+      })
+    })
+  }
+}))
+//////////////SIGNUP-SIGNUP-SIGNUP////////////////////////////
+
+
+passport.use('local-login', new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: true},
+  function(req, email, password, done){
+    User.findOne({'local.email': email,}, function(err, user){
+      if(err) throw err
+      //if they there is no user then do this
+      if(!user) return done(null, false, req.flash('loginMessage', 'No User Found. bleh!!'))
+    })
+  }
+))
